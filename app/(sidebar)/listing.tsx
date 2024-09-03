4f6980fe56adaa5798f7f527f489af2ecf3ea2da
haziq <haziq.musa02@gmail.com>
2024-09-04 02:25:37 +0800

    created page

--- app/(sidebar)/listing.tsx
+++ app/(sidebar)/listing.tsx
@@ -1,10 +1,23 @@
-import { View, Text } from "native-base";
+import CardItem from "@/components/CardItem";
+import { CARD_ITEM } from "@/constants/Data";
+import { Box, FlatList } from "native-base";
 import React from "react";
 
 export default function Listing() {
   return (
-    <View>
-      <Text>Listing</Text>
-    </View>
+    <Box>
+      <FlatList
+        mt="3"
+        style={{ columnGap: 200 }}
+        data={CARD_ITEM}
+        renderItem={({ item }) => (
+          <Box mb="3">
+            <CardItem {...item} />
+          </Box>
+        )}
+        numColumns={2}
+        columnWrapperStyle={{ justifyContent: "space-evenly" }}
+      />
+    </Box>
   );
 }
--- app/(sidebar)/location.tsx
+++ app/(sidebar)/location.tsx
@@ -1,10 +1,19 @@
 import { View, Text } from "native-base";
 import React from "react";
+import MapView from "react-native-maps";
 
 export default function Location() {
   return (
     <View>
-      <Text>Location</Text>
+      <MapView
+        style={{ width: "100%", height: "100%" }}
+        initialRegion={{
+          latitude: 5.33659,
+          longitude: 103.141998,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </View>
   );
 }
