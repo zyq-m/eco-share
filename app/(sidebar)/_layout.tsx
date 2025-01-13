510c888f84f7c59a9a4512748cff747365174565
haziq <haziq.musa02@gmail.com>
2025-01-13 08:20:54 +0800

    add custome header

--- app/(sidebar)/_layout.tsx
+++ app/(sidebar)/_layout.tsx
@@ -1,65 +1,77 @@
 import { Drawer } from "expo-router/drawer";
 
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { Text } from "native-base";
 
 export default function AppLayout() {
-  return (
-    <Drawer>
-      <Drawer.Screen
-        name="(tabs)"
-        options={{
-          title: "Home",
-          drawerIcon: () => (
-            <MaterialIcons name="home" size={24} color="black" />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="listing"
-        options={{
-          title: "My Listings",
-          drawerIcon: () => (
-            <MaterialIcons name="list" size={24} color="black" />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="nearMe"
-        options={{
-          title: "Near me",
-          drawerIcon: () => (
-            <MaterialIcons name="near-me" size={24} color="black" />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="helpCentre"
-        options={{
-          title: "Guidelines",
-          drawerIcon: () => (
-            <MaterialIcons name="question-mark" size={24} color="black" />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="aboutUs"
-        options={{
-          title: "About Us",
-          drawerIcon: () => (
-            <MaterialIcons name="question-mark" size={24} color="black" />
-          ),
-        }}
-      />
-      <Drawer.Screen
-        name="termCondition"
-        options={{
-          title: "Terms & conditions",
-          drawerIcon: () => (
-            <MaterialIcons name="question-mark" size={24} color="black" />
-          ),
-        }}
-      />
-    </Drawer>
-  );
+	return (
+		<Drawer>
+			<Drawer.Screen
+				name="(tabs)"
+				options={{
+					title: "Home",
+					drawerIcon: () => (
+						<MaterialIcons name="home" size={24} color="black" />
+					),
+					headerShown: false,
+				}}
+			/>
+			<Drawer.Screen
+				name="listing"
+				options={{
+					title: "My Listings",
+					drawerIcon: () => (
+						<MaterialIcons name="list" size={24} color="black" />
+					),
+				}}
+			/>
+			<Drawer.Screen
+				name="nearMe"
+				options={{
+					title: "Near me",
+					drawerIcon: () => (
+						<MaterialIcons name="near-me" size={24} color="black" />
+					),
+				}}
+			/>
+			<Drawer.Screen
+				name="helpCentre"
+				options={{
+					title: "Guidelines",
+					drawerIcon: () => (
+						<MaterialIcons
+							name="question-mark"
+							size={24}
+							color="black"
+						/>
+					),
+				}}
+			/>
+			<Drawer.Screen
+				name="aboutUs"
+				options={{
+					title: "About Us",
+					drawerIcon: () => (
+						<MaterialIcons
+							name="question-mark"
+							size={24}
+							color="black"
+						/>
+					),
+				}}
+			/>
+			<Drawer.Screen
+				name="termCondition"
+				options={{
+					title: "Terms & conditions",
+					drawerIcon: () => (
+						<MaterialIcons
+							name="question-mark"
+							size={24}
+							color="black"
+						/>
+					),
+				}}
+			/>
+		</Drawer>
+	);
 }
---
+++ components/HomeHeader.tsx
@@ -0,0 +1,147 @@
+import React from "react";
+import {
+	Actionsheet,
+	Box,
+	HStack,
+	Icon,
+	IconButton,
+	Input,
+	Text,
+	useDisclose,
+} from "native-base";
+import { router, useNavigation } from "expo-router";
+
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { useRoute } from "@react-navigation/native";
+
+export default function HomeHeader() {
+	const navigation = useNavigation();
+	const route = useRoute();
+	const { isOpen, onOpen, onClose } = useDisclose();
+
+	return (
+		<Box safeAreaTop bg="primary.500">
+			<HStack bg="primary.500" px="1" py="3" alignItems="center">
+				<IconButton
+					icon={
+						<Icon
+							as={MaterialIcons}
+							name="menu"
+							size="lg"
+							color="white"
+						/>
+					}
+					onPress={() => navigation.openDrawer()}
+				/>
+				<Input
+					px="4"
+					py="1"
+					flex="1"
+					variant="rounded"
+					backgroundColor="white"
+					placeholder="Search"
+					InputRightElement={
+						<Icon
+							as={AntDesign}
+							name="search1"
+							mr="4"
+							color="muted.400"
+						/>
+					}
+				/>
+				{route.name !== "search" ? (
+					<>
+						<IconButton
+							icon={
+								<Icon
+									as={FontAwesome5}
+									name="cart-plus"
+									color="white"
+								/>
+							}
+							onPress={() => router.push("/(item)/requestList")}
+						/>
+						<IconButton
+							icon={
+								<Icon
+									as={AntDesign}
+									name="heart"
+									color="white"
+								/>
+							}
+							onPress={() => router.push("/(item)/requestList")}
+						/>
+					</>
+				) : (
+					<>
+						<IconButton
+							icon={
+								<Icon
+									as={FontAwesome5}
+									name="filter"
+									color="white"
+								/>
+							}
+							onPress={onOpen}
+						/>
+						<Actionsheet isOpen={isOpen} onClose={onClose}>
+							<Actionsheet.Content>
+								<Box
+									w="100%"
+									h={60}
+									px={4}
+									justifyContent="center"
+								>
+									<Text
+										fontSize="16"
+										color="gray.500"
+										_dark={{
+											color: "gray.300",
+										}}
+									>
+										Filter by
+									</Text>
+								</Box>
+								<Actionsheet.Item
+									startIcon={
+										<Icon
+											as={FontAwesome5}
+											name="map-marker-alt"
+											size="6"
+										/>
+									}
+								>
+									Nearest
+								</Actionsheet.Item>
+								<Actionsheet.Item
+									startIcon={
+										<Icon
+											as={FontAwesome5}
+											name="globe"
+											size="6"
+										/>
+									}
+								>
+									Latest post
+								</Actionsheet.Item>
+								<Actionsheet.Item
+									startIcon={
+										<Icon
+											as={FontAwesome5}
+											name="calendar-day"
+											size="6"
+										/>
+									}
+								>
+									List for
+								</Actionsheet.Item>
+							</Actionsheet.Content>
+						</Actionsheet>
+					</>
+				)}
+			</HStack>
+		</Box>
+	);
+}
